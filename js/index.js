ecfd402c578170e6495bb77267cdc2730fc705b6
fan-sir666 <fan_sir666@163.com>
2020-04-23 22:55:54 +0800

    添加：绿点

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
+/* header 下登录区域 左边的定位显示 */
+$('.register-location > a').on('mouseenter', function() {
+    $('.register-location').addClass('active');
+    $('.register-area').show();
+})
+$('.register-left').on('mouseleave', function() {
+    $('.register-location').removeClass('active');
+    $('.register-area').hide();
+})
 
+/* header 下登录区域 右侧的下拉显示 */
+$('.register-item').on('mouseenter', function() {
+    $(this).addClass('active');
+    $(this).children('ul').show();
+})
+$('.register-item').on('mouseleave', function() {
+    $(this).removeClass('active');
+    $(this).children('ul').hide();
+})
+
+/* header 下登录区域 右侧的联系管家表单显示 */
+$('.register-four').on('mouseenter', function() {
+    $(this).children('.register-four-hide').show();
+})
+$('.register-four').on('mouseleave', function() {
+    $(this).children('.register-four-hide').hide();
+})
+
+/* 显示 */
+$('.search-item').on('mouseenter', function() {
+    $(this).children('.search-cont').show();
+})
+$('.search-item').on('mouseleave', function() {
+    $(this).children('.search-cont').hide();
+})
